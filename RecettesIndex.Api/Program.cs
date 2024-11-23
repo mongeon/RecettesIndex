6018bae1251bf0b9fe1884e86f6d3c2a94ea66ee
Gabriel Mongeon <gabrielmongeon@hotmail.com>
2024-11-23 16:25:57 -0500

    Update Supabase env var names for configuration
    Updated the environment variable names used in the `SupabaseConfiguration` object. Changed `Supabase.Url` to `SUPABASE_URL` and `Supabase.Key` to `SUPABASE_KEY` to ensure the application retrieves the Supabase URL and key from the new environment variable names.

--- RecettesIndex.Api/Program.cs
+++ RecettesIndex.Api/Program.cs
@@ -17,11 +17,10 @@ var builder = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        var b = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
         var supabaseConfig = new SupabaseConfiguration
         {
-            Url = Environment.GetEnvironmentVariable("Supabase.Url", EnvironmentVariableTarget.Process) ?? string.Empty,
-            Key = Environment.GetEnvironmentVariable("Supabase.Key", EnvironmentVariableTarget.Process) ?? string.Empty
+            Url = Environment.GetEnvironmentVariable("SUPABASE_URL", EnvironmentVariableTarget.Process) ?? string.Empty,
+            Key = Environment.GetEnvironmentVariable("SUPABASE_KEY", EnvironmentVariableTarget.Process) ?? string.Empty
         };
 
         services.AddSingleton(provider => new Supabase.Client(supabaseConfig.Url ?? string.Empty, supabaseConfig.Key ?? string.Empty, options));
