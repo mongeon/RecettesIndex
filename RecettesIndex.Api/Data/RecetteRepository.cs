c2965839458ff26a0f9e1c7b37d0a2fc9cc07f64
Gabriel Mongeon <gabrielmongeon@gmail.com>
2024-11-30 23:29:23 -0500

    Add GetRecette method to IRecetteRepository and RecetteRepository, and implement GetRecette function in RecettesFunction

--- RecettesIndex.Api/Data/RecetteRepository.cs
+++ RecettesIndex.Api/Data/RecetteRepository.cs
@@ -12,7 +12,15 @@ public class RecetteRepository(Supabase.Client client) : IRecetteRepository
 
         return result.Models;
     }
+    public async Task<Recette?> GetRecette(int id)
+    {
+        var result = await client
+            .From<Recette>()
+            .Where(b => b.Id == id)
+            .Single();
 
+        return result;
+    }
     public async Task<Recette?> Insert(Recette recette)
     {
         var result = await client.From<Recette>().Insert(recette);
