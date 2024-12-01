5e8e56e0164404926dba546c9d2e203b5da92cd8
Gabriel Mongeon <gabrielmongeon@gmail.com>
2024-11-30 22:08:12 -0500

    Add AuthorRepository to DI container

---
+++ RecettesIndex.Api/Data/Converter/AuthorConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace RecettesIndex.Api.Data.Converter;
+
+public static class AuthorConverter
+{
+    public static Models.Author Convert(this Shared.Author author)
+    {
+        return new Models.Author
+        {
+            Id = author.Id,
+            Name = author.Name,
+            CreatedAt = author.CreatedAt
+        };
+    }
+    public static Shared.Author? Convert(this Models.Author author)
+    {
+        if (author == null)
+        {
+            return null;
+        }
+
+        return new Shared.Author
+        {
+            Id = author.Id,
+            Name = author.Name,
+            CreatedAt = author.CreatedAt
+        };
+    }
+}
---
+++ RecettesIndex.Shared/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace RecettesIndex.Shared;
+
+public record Author
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public string FullName { get{ return  $"{FirstName} {LastName}";}}
+
+    public DateTime CreatedAt { get; set; }
+}
