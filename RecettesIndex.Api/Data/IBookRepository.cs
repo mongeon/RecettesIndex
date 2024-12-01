1ab53225e6f0bbcd9fef8692faddbfbfae3575ed
Gabriel Mongeon <gabrielmongeon@hotmail.com>
2024-12-01 12:03:33 -0500

    Add method to fetch books by author and update UI
    - Added `GetBooksByAuthor` method in `BookRepository` to fetch books by author ID.
    - Updated `IBookRepository` interface to include `GetBooksByAuthor`.
    - Introduced `RunGetBooksByAuthor` function in `BooksFunction` to handle HTTP GET requests for books by author ID.
    - Modified `AuthorDetails.razor` to display books by the author, fetching data from the API during initialization.

--- RecettesIndex.Api/Data/IBookRepository.cs
+++ RecettesIndex.Api/Data/IBookRepository.cs
@@ -6,5 +6,6 @@ public interface IBookRepository
 {
     Task<Book?> GetBook(int id);
     Task<IEnumerable<Book>> GetBooks();
+    Task<IEnumerable<Book>> GetBooksByAuthor(int authorId);
     Task<Book?> Insert(Book book);
 }
