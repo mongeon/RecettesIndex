d2abea7720002642807f9735dc168bed5e04143f
Gabriel Mongeon <gabrielmongeon@hotmail.com>
2024-12-09 07:54:09 -0500

    Integrate Blazorise and update UI components
    - Updated `App.razor` to include Blazorise components and custom theme.
    - Modified `MainLayout.razor` to use Blazorise layout components.
    - Updated `Program.cs` to configure Blazorise services with Tailwind CSS and FontAwesome icons.
    - Added new package references in `RecettesIndex.csproj` for Blazorise.
    - Updated `_Imports.razor` to include the `Blazorise` namespace.
    - Changed font family to "Inter" and removed custom styles in `app.css`.
    - Updated `index.html` to use external CSS/JS libraries and removed local resources.
    - Enhanced `SideMenu.razor` and `TopMenu.razor` with new navigation bars and icons.
    - Set up `RootLayout.razor` to use a layout component base.
    - Cleared content of `weather.json`.

--- RecettesIndex/Program.cs
+++ RecettesIndex/Program.cs
@@ -1,3 +1,7 @@
+using Blazorise;
+using Blazorise.Icons.FontAwesome;
+using Blazorise.LoadingIndicator;
+using Blazorise.Tailwind;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RecettesIndex;
@@ -6,6 +10,13 @@ using RecettesIndex.Data;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services
+    .AddBlazorise()
+    .AddTailwindProviders()
+    .AddFontAwesomeIcons();
+
+builder.Services.AddLoadingIndicator();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
