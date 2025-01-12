f34d013af3d1d6a0ad2d5f4f5db560f5a697869d
Gabriel Mongeon <gabrielmongeon@hotmail.com>
2025-01-12 13:47:54 -0500

    Refactor recipe fetching by author
    Removed old commented-out code for fetching recipes by author. Updated the query to use `book_id_info:books(authors!inner(id))` syntax, filtering by `authorId`. Adjusted the return statement to ensure only recipes with books containing the specified author are included.

--- RecettesIndex.Api/Data/RecetteRepository.cs
+++ RecettesIndex.Api/Data/RecetteRepository.cs
@@ -42,17 +42,13 @@ public class RecetteRepository(Supabase.Client client) : IRecetteRepository
 
     public async Task<IEnumerable<Recette>> GetRecettesByAuthor(int authorId)
     {
-        //var result = await client
-        //    .From<Recette>()
-        //    .Select("*, book:book_id(*, author(*))")
-        //    .Filter("book.author.id", Supabase.Postgrest.Constants.Operator.Equals, authorId)
-        //    .Get();
-        //return result.Models.Where(x => x.Book?.Authors.Any(a => a.Id == authorId) ?? false);
 
         var result = await client.From<Recette>()
-            .Select("*,  Author_info:authors!inner(*)")
-            .Filter("authors.id", Constants.Operator.Equals, authorId)
+            .Select("*, book_id_info:books(authors!inner(id))")
+            .Filter("book_id_info.authors.id", Constants.Operator.Equals, authorId)
             .Get();
-        return result.Models;
+        return result
+            .Models
+            .Where(x => x.Book != null && x.Book.Authors.Any(a => a.Id == authorId));
     }
 }
